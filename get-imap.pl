078f72a515fde4adea27fd71f17185fb15108ec5
eitan adler <eitan.adler@gmail.com>
2010-05-09 19:51:36 +0300

    We are close to getting to only the important folder

--- get-imap.pl
+++ get-imap.pl
@@ -30,8 +30,31 @@ my $client = Mail::IMAPClient->new(
 
 # Listing all of your folders
 print "I'm authenticated\n" if $client->IsAuthenticated();
-my @folders = $client->folders();
-print join("\n* ", 'Folders:', @folders), "\n";
+#my @folders = $client->folders();
+#print join("\n* ", 'Folders:', @folders), "\n";
+
+my $msgcount = $client->message_count($Conf::conf{"folder.todo"}); 
+defined($msgcount) or die "Could not message_count: $@\n";
+print "We have $msgcount unread messages in ". $Conf::conf{"folder.todo"}."\n";
+
+if ($msgcount > 0)
+{
+	print "Attempting to select a folder....";
+	if ($client->selectable($Conf::conf{"folder.todo"}))
+	{
+		$client->select($Conf::conf{"folder.todo"}) or die "Could not select: $@\n"	;
+		print "done\n";
+	}
+	else
+	{
+		print "but we can't";
+	}
+}
+else
+{
+	print "Not selecting any folder with 0 messages";
+}
 
 # Say goodbye
 $client->logout();
+print "\n";
